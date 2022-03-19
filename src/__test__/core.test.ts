768f2ece3527d356f8711bec9aa4c9f9251b0c23
czh740132583 <740132583@qq.com>
2022-03-19 14:11:39 +0800

    feat: 添加单测

--- src/__test__/core.test.ts
+++ src/__test__/core.test.ts
@@ -2,9 +2,17 @@
  * 测试props生效
  * @param options
  */
-describe('test-be-breadcrumb-props', () => {
-    test('props-to', () => {
-        const asd = 'A'
-        expect(asd).toBe('A')
+import {camelize} from "../utils/parser";
+import {findSvgFile} from "../utils/svg-file-loader";
+import path from 'path'
+import {ISvgCtxItem} from "../types";
+describe('vite-plugin-svg-transform-script', () => {
+    test('test-camelize ', () => {
+        const testStr = 'aaaa-vvvv'
+        expect(camelize(testStr)).toBe('aaaaVvvv')
+    })
+    test('test-findSvgFile ', () => {
+        const fileInfo:Array<ISvgCtxItem> = findSvgFile(path.resolve() + '/src/__test__/')
+        expect(fileInfo[0].name).toBe('account-book')
     })
 })
